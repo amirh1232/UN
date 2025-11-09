f4397fef9a79802805fdc306f667468793df3a20
amirh1232 <136537873+amirh1232@users.noreply.github.com>
2025-11-09 23:30:25 +0330

    Add files via upload

---
+++ T1.Q2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1.Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                float temp;
+                Console.WriteLine("Enter your room Temperature in Celsius or A-Z to exit");
+                try
+                {
+                    temp = float.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    break;
+                }
+                Console.WriteLine("Your Temperature is " + ((temp*9.8)+32) +" Farenheit");
+
+            }
+        }
+    }
+}
---
+++ T2.Q1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2.Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter yor Number");
+            int x;
+            x = Convert.ToInt16(Console.ReadLine());
+            if( x<10 && x %2 == 0)
+            {
+                Console.WriteLine("True !");
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}

70e5f26045b562d5f0c3d11e4ac4c36c99d47686
amirh1232 <136537873+amirh1232@users.noreply.github.com>
2025-11-09 23:28:47 +0330

    Add files via upload

---
+++ T1.Q1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1.Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                float r;
+                Console.WriteLine("inter your Circle's radius Or 0 for Exit");
+                r = float.Parse(Console.ReadLine());
+                if (r <= 0)
+                {
+                    break;
+                }
+                float s, p;
+                s = (float)r * r;
+                s = (float)(s * 3.14);
+                p = (float)( r * 3.14 *2);
+                Console.WriteLine("Masahat = " + s );
+                Console.WriteLine("Mohit = " + p);
+            }
+        }
+    }
+}

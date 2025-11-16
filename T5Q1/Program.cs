67b97a0b4662084e0329d15660e92c0954229ac9
amirh1232 <136537873+amirh1232@users.noreply.github.com>
2025-11-16 20:47:49 +0330

    Add files via upload

---
+++ T5Q1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T5Q1
+{
+    internal class Program
+    {
+        public static float mohit(float x,float y)
+        {
+            float z = (float)Math.Sqrt((x*x) + (y*y));
+            return (x + y + z);
+        }
+        public static float masahat(float x, float y)
+        {
+            return x * y/2;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("do zele ghaem mosalas ghaem khod ra vared konid ");
+            float x, y,P,S;
+            x = float.Parse(Console.ReadLine());
+            y = float.Parse(Console.ReadLine());
+            P = mohit(x, y);
+            S = masahat(x, y);
+            Console.WriteLine("Mohit mosalas =" + P);
+            Console.WriteLine("Masahat mosalas =" + S);
+            Console.ReadKey();
+        }
+    }
+}
